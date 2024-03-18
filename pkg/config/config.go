67de13af47f1ce70f5452afcef51a8d6287c9d7e
Naff <ahmadnafim30@gmail.com>
2024-03-18 21:35:42 +0700

    update config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,13 +2,16 @@ package config
 
 import (
 	"log"
-	// "os"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
-func LoadEnv() {
-	if err := godotenv.Load("../../.env"); err != nil {
+func LoadEnv() error {
+	err := godotenv.Load()
+	env := os.Getenv("ENV")
+	if err != nil && env == "" {
 		log.Fatalf("error load env : %v", err)
 	}
+	return nil
 }
